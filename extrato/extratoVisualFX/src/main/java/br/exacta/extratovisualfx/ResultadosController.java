e773e7b8367318295a8a633f7dfd36b96a3a5bf3
Thales Tassi <thales.tassi@exactabalancas.com.br>
2017-12-28 19:11:55 -0200

    Botão importar resultados json

--- extrato/extratoVisualFX/src/main/java/br/exacta/extratovisualfx/ResultadosController.java
+++ extrato/extratoVisualFX/src/main/java/br/exacta/extratovisualfx/ResultadosController.java
@@ -5,9 +5,19 @@
  */
 package br.exacta.extratovisualfx;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -16,12 +26,47 @@ import javafx.fxml.Initializable;
  */
 public class ResultadosController implements Initializable {
 
+    @FXML
+    private Text lblNome;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private AnchorPane apAcoes;
+    @FXML
+    private Text lblAcoes;
+    @FXML
+    private VBox vbBotoes;
+    @FXML
+    private Button btnImportar;
+    @FXML
+    private Button btnExportar;
+    @FXML
+    private Text lblLista;
+    @FXML
+    private Text lblDados;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // PARA IMPORTAR ARQUIVO JSON
+        btnImportar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+                // SEQUENCIA DE PASSOS PARA ABRIR UMA CAIXA DE DIALOGO ESCOLHENDO O ARQUIVO DE EXTENSÃO JSON
+                FileChooser escolherArquivo = new FileChooser();
+                escolherArquivo.setTitle("Selecione o arquivo JSON");
+                escolherArquivo.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON", "*.json"));
+                File arquivo = escolherArquivo.showOpenDialog(null);
+                
+                // MANIPULACAO DO ARQUIVO JSON ESCOLHIDO
+                //ObjectMapper objectMapper = new ObjectMapper();
+            }
+        });
+
+        // PARA EXPORTAR PARA CSV        
+    }
+
 }
